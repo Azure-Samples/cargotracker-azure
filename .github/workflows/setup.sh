37461082bff4d11fa05f4e6d47d39de77971cb77
Ed Burns <edburns@microsoft.com>
2021-10-14 20:52:03 -0400

    On branch main modified:   .github/workflows/setup.sh
    - Echo some things.
    
    - VERY IMPORTANT: The value of the `USER_ASSIGNED_MANAGED_IDENTITY_ID`
      secret must not have leading and trailing quotes.
    
    modified:   .github/workflows/setupWlsAks.yml
    
    - Added `az deployment group validate` before `create`. Fail fast.
    
    Signed-off-by: Ed Burns <edburns@microsoft.com>

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -159,6 +159,8 @@ az role assignment create --role Contributor --assignee-principal-type ServicePr
 
 # https://stackoverflow.com/questions/13210880/replace-one-substring-for-another-string-in-shell-script
 USER_ASSIGNED_MANAGED_IDENTITY_ID=${USER_ASSIGNED_MANAGED_IDENTITY_ID_NOT_ESCAPED//\//\\/}
+# remove leading and trailing quote
+USER_ASSIGNED_MANAGED_IDENTITY_ID=${USER_ASSIGNED_MANAGED_IDENTITY_ID//\"/}
 
 msg "${GREEN}(6/6) Create secrets in GitHub"
 if $USE_GITHUB_CLI; then
@@ -166,11 +168,17 @@ if $USE_GITHUB_CLI; then
     msg "${GREEN}Using the GitHub CLI to set secrets.${NOFORMAT}"
     gh ${GH_FLAGS} secret set AKS_REPO_USER_NAME -b"${AKS_REPO_USER_NAME}"
     gh ${GH_FLAGS} secret set AZURE_CREDENTIALS -b"${AZURE_CREDENTIALS}"
+    msg "${YELLOW}\"AZURE_CREDENTIALS\""
+    msg "${GREEN}${AZURE_CREDENTIALS}"
     gh ${GH_FLAGS} secret set DB_PASSWORD -b"${DB_PASSWORD}"
     gh ${GH_FLAGS} secret set ORC_SSOPSW -b"${ORC_SSOPSW}"
     gh ${GH_FLAGS} secret set ORC_SSOUSER -b"${ORC_SSOUSER}"
     gh ${GH_FLAGS} secret set SERVICE_PRINCIPAL -b"${SERVICE_PRINCIPAL}"
+    msg "${YELLOW}\"SERVICE_PRINCIPAL\""
+    msg "${GREEN}${SERVICE_PRINCIPAL}"
     gh ${GH_FLAGS} secret set USER_ASSIGNED_MANAGED_IDENTITY_ID -b"${USER_ASSIGNED_MANAGED_IDENTITY_ID}"
+    msg "${YELLOW}\"USER_ASSIGNED_MANAGED_IDENTITY_ID\""
+    msg "${GREEN}${USER_ASSIGNED_MANAGED_IDENTITY_ID}"
     gh ${GH_FLAGS} secret set WDT_RUNTIMEPSW -b"${WDT_RUNTIMEPSW}"
     gh ${GH_FLAGS} secret set WLS_PSW -b"${WLS_PSW}"    
     gh ${GH_FLAGS} secret set WLS_USERNAME -b"${WLS_USERNAME}"    
