91bde24136e414c328dbe7b88f4175c0b1812f23
Zheng Chang <zhengchang@microsoft.com>
2022-02-14 16:25:00 +0800

    use shell scripts instead of antrun plugin to avoid conflicts

---
+++ .github/workflows/createOpenLibertyLocalFiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+script="${BASH_SOURCE[0]}"
+scriptDir="$(cd "$(dirname "${script}")" && pwd)"
+
+repoRootDir="$(dirname "$(dirname ${scriptDir})")"
+
+echo $repoRootDir
+
+mv ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/JmsQueueNames.java ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/JmsQueueNames.java.moved
+cp ${repoRootDir}/src/main/liberty/jms/JmsQueueNames.java ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/
+
+mv ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/jms/JmsApplicationEvents.java ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/jms/JmsApplicationEvents.java.moved
+cp ${repoRootDir}/src/main/liberty/jms/JmsApplicationEvents.java ${repoRootDir}/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/jms/
+
+mv ${repoRootDir}/src/main/webapp/WEB-INF/web.xml ${repoRootDir}/src/main/webapp/WEB-INF/web.xml.moved
+cp ${repoRootDir}/src/main/liberty/webapp/WEB-INF/web.xml ${repoRootDir}/src/main/webapp/WEB-INF/
