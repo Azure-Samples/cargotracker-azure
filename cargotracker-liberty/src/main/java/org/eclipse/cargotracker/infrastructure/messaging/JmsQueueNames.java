995cdc05be7ccec8bd3423d6c8d4e144b6b8712d
zhengchang907 <zhengchang@microsoft.com>
2022-03-01 01:34:36 +0800

    Make Liberty on AKS pipelines work in demo branch (#2)
    * Disable SSL for PostgreSQL
    
    * Remove unused files and add parameters.json for deploying Liberty on AKS
    
    * make updateOpenLiberty pipeline works with new file structure
    
    * add sleep time for first deployment
    
    * applied required changes for OpenLiberty

--- cargotracker-liberty/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/JmsQueueNames.java
+++ cargotracker-liberty/src/main/java/org/eclipse/cargotracker/infrastructure/messaging/JmsQueueNames.java
@@ -1,13 +1,15 @@
 package org.eclipse.cargotracker.infrastructure.messaging;
 
+// for Open liberty.
+// removed the global prefix.
 public class JmsQueueNames {
-    public static final String CARGO_HANDLED_QUEUE = "java:app/jms/CargoHandledQueue";
-    public static final String MISDIRECTED_CARGO_QUEUE = "java:app/jms/MisdirectedCargoQueue";
-    public static final String DELIVERED_CARGO_QUEUE = "java:app/jms/DeliveredCargoQueue";
+    public static final String CARGO_HANDLED_QUEUE = "jms/CargoHandledQueue";
+    public static final String MISDIRECTED_CARGO_QUEUE = "jms/MisdirectedCargoQueue";
+    public static final String DELIVERED_CARGO_QUEUE = "jms/DeliveredCargoQueue";
     public static final String REJECTED_REGISTRATION_ATTEMPTS_QUEUE =
-            "java:app/jms/RejectedRegistrationAttemptsQueue";
+            "jms/RejectedRegistrationAttemptsQueue";
     public static final String HANDLING_EVENT_REGISTRATION_ATTEMPT_QUEUE =
-            "java:app/jms/HandlingEventRegistrationAttemptQueue";
+            "jms/HandlingEventRegistrationAttemptQueue";
 
     private JmsQueueNames() {}
 }
